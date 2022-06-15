017ebb76449a6ca3f3432d742414e31c20b1a40e
ardhan <ardhan.s.pratama@gmail.com>
2022-06-15 16:23:08 +0800

    Added UI elements to login page

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,12 @@
 import { useRef, useState, useEffect, useContext } from "react"
-import AuthContext from "./context/AuthPrvoider";
-import axios from "./api/axios";
-const LOGIN_URL = "/auth";
+// import AuthContext from "./context/AuthPrvoider";
+// import axios from "./api/axios";
+import logo1 from "./images/app_icon.png";
+import logo2 from "./images/login_img.png";
+// const LOGIN_URL = "/auth";
 
 const Login = () => {
-    const { setAuth } = useContext(AuthContext);
+    // const { setAuth } = useContext(AuthContext);
     const userRef = useRef();
     const errRef = useRef();
 
@@ -24,36 +26,36 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try{
-            const response = await axios.post(LOGIN_URL, 
-                JSON.stringify({user, pwd}),
-                {
-                    headers: {'Content-Type': 'application/json'},
-                    withCredentials: true
-                }
-            );
-            console.log(JSON.stringify(response?.data));
+        // try {
+        //     const response = await axios.post(LOGIN_URL, 
+        //         JSON.stringify({user, pwd}),
+        //         {
+        //             headers: {'Content-Type': 'application/json'},
+        //             withCredentials: true
+        //         }
+        //     );
+        //     console.log(JSON.stringify(response?.data));
 
-            //console.log(user, pwd); // see user login detail
-            const accessToken = response?.data?.accessToken;
-            const roles = response?.data?.roles;
-            setAuth({ user, pwd, roles, accessToken });
-            setUser('');
-            setPwd('');
-            setSuccess(true);
+        //     //console.log(user, pwd); // see user login detail
+        //     const accessToken = response?.data?.accessToken;
+        //     const roles = response?.data?.roles;
+        //     setAuth({ user, pwd, roles, accessToken });
+        setUser('');
+        setPwd('');
+        setSuccess(true);
 
-        } catch (err) {
-            if (!err?.response) {
-                setErrMsg('No Server Response');
-            } else if (err.response?.status === 400) {
-                setErrMsg('Missing Username or Password');
-            } else if (err.response?.status === 401) {
-                setErrMsg('Unauthorized');
-            } else {
-                setErrMsg('Login Failed');
-            }
-            errRef.current.focus();
-        }
+        // } catch (err) {
+        //     if (!err?.response) {
+        //         setErrMsg('No Server Response');
+        //     } else if (err.response?.status === 400) {
+        //         setErrMsg('Missing Username or Password');
+        //     } else if (err.response?.status === 401) {
+        //         setErrMsg('Unauthorized');
+        //     } else {
+        //         setErrMsg('Login Failed');
+        //     }
+        //     errRef.current.focus();
+        // }
     }
 
     return (
@@ -61,41 +63,52 @@ const Login = () => {
             {success ? (
                 <section>
                     <h1>You are logged in!</h1>
-                    <br />
+                    <br/>
+
                     <p>
-                        <a href="#">Go to Home</a>
+                        <a href="Login">Go to Home</a>
                     </p>
                 </section>
             ) : (
+                <div id="login-box">
+                    <section id="left-box">
+                        <img src={logo1} alt="Application Logo" id="app-logo"/>
+                        <img src={logo2} alt="Example Image"id="example-img"/>
+                        <h2>Monitor your business with our software.</h2>
+                        <h2>Powered by Apache Kafka.</h2>
+                    </section>
 
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h1>Log in to WareEyes</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Username:</label>
-                        <input type="text" 
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                        />
-                        <label htmlFor="password">Password:</label>
-                        <input
+                    <section id="right-box">
+                        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                        <h1 id="login-title">Log in to WareEyes</h1>
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="username">Username</label>
+                            <input type="text" 
+                                id="username"
+                                placeholder="Enter username..."
+                                ref={userRef}
+                                autoComplete="off"
+                                onChange={(e) => setUser(e.target.value)}
+                                value={user}
+                                required
+                            />
+                            <label htmlFor="password">Password</label>
+                            <input
                                 type="password"
                                 id="password"
+                                placeholder="Enter password..."
                                 onChange={(e) => setPwd(e.target.value)}
                                 value={pwd}
                                 required
-                        />
-                    </form>
-                    <p>
-                        {/*put router link here*/}
-                        <a href="#">forgot password?</a>
-                    </p>
-                    <button>Log in</button>
-                </section>
+                            />
+                            <p>
+                                {/*put router link here*/}
+                                <a href="#">Forgot password?</a>
+                            </p>
+                            <button>Login</button>
+                        </form>
+                    </section>
+                </div>
                 )}
         </>
     )
