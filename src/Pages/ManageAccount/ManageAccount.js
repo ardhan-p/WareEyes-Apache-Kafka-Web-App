20c7ec281adeb4d5fa1516b27f8aa5a2916aeec5
RayanLio <rayanljy@gmail.com>
2022-08-02 19:34:11 +0800

    added create user account UI

--- src/Pages/ManageAccount/ManageAccount.js
+++ src/Pages/ManageAccount/ManageAccount.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import Navbar from "../../Components/Navbar/Navbar";
+import Popup from "../../Components/Popup/Popup";
 import { DataGrid } from "@mui/x-data-grid";
 import "./ManageAccount.css";
 
@@ -9,24 +10,97 @@ function ManageAccount() {
   let navigate = useNavigate();
 
   const columns = [
-    { field: "firstName", headerName: "Name", width: 130 },
-    { field: "email", headerName: "Email", width: 130 },
+    { field: "firstName", headerName: "Name", width: 230 },
+    { field: "email", headerName: "Email", width: 330 },
     { field: "role", headerName: "Role", width: 130 },
+    { field: "dateCreated", headerName: "Date created", width: 230 },
+    { field: "permission", headerName: "Permission", width: 130 },
+    { field: "remark", headerName: "Remark", width: 380 },
   ];
 
   const rows = [
-    { id: 1, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 2, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 3, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 4, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 5, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 6, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 7, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 8, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 9, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
-    { id: 10, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" } ,
-    { id: 11, firstName: 'Ryan', email: 'ryan@gmail.com', role: "user" },
+    {
+      id: 1,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+      remark: "This account is created recently",
+    },
+    {
+      id: 2,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+      remark: "This account is not admin and only got a limite feature",
+    },
+    {
+      id: 3,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
+    {
+      id: 4,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
+    {
+      id: 5,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
+    {
+      id: 6,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
+    {
+      id: 7,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
+    {
+      id: 8,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
+    {
+      id: 9,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
+    {
+      id: 10,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
+    {
+      id: 11,
+      firstName: "Ryan",
+      email: "ryan@gmail.com",
+      role: "user",
+      dateCreated: "May 21, 2022",
+    },
   ];
+
+  const [buttonPopup, setButtonPopup] = useState(false);
   return (
     <div className="manageAcc-page">
       <Sidebar />
@@ -44,7 +118,29 @@ function ManageAccount() {
             Back
           </button>
         </div>
-        <div style={{ height: 476, width: "70%", margin:50 }}>
+        <div className="add-delete">
+          <button
+            type="button"
+            className="add-btn"
+            onClick={() => {
+              setButtonPopup(true);
+            }}
+          >
+            Add
+          </button>
+          <button
+            type="button"
+            className="delete-btn"
+            onClick={() => {
+              navigate("/AdminTools");
+            }}
+          >
+            Delete
+          </button>
+        </div>
+        <div
+          style={{ height: 476, width: "90%", marginLeft: 50, marginTop: 20 }}
+        >
           <DataGrid
             rows={rows}
             columns={columns}
@@ -54,6 +150,35 @@ function ManageAccount() {
           />
         </div>
       </div>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h2>Account Creation</h2>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>
+              <input type="text"></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Username</th>
+            <th>
+              <input type="text"></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Password</th>
+            <th>
+              <input type="text"></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Role</th>
+            <th>
+              <input type="text"></input>
+            </th>
+          </tr>
+        </table>
+      </Popup>
     </div>
   );
 }
