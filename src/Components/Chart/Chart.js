11175387a0826f24ed3460f75d8187ff34966afd
RayanLio <rayanljy@gmail.com>
2022-08-15 01:10:42 +0800

    edited the chart UI and start to post and get data from database

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -3,67 +3,77 @@ import "./Chart.css";
 import { Line } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
 import "chartjs-adapter-date-fns";
-Chart.register(...registerables);
+import DataLabelsPlugin from "chartjs-plugin-datalabels";
+Chart.register(...registerables, DataLabelsPlugin);
 
 function Graph({ topicTitle, topicThreshold }) {
   let ref = useRef(null);
 
-  // date formate
-  var defaultStartDate = (new Date()).toISOString().split('T')[0];
-  var tempDate = (new Date());
-  tempDate.setDate(tempDate.getDate() + 7);
-  var defaultEndDate = tempDate.toISOString().split('T')[0];
+  const [selectedDate, setSelectedDate] = useState("");
 
-  // to export the graph into image
-  const downloadGraph = useCallback(() => {
-    const link = document.createElement("a");
-    link.download = topicTitle + ".jpeg";
-    link.href = ref.current.toBase64Image("image/jpeg", 1);
-    link.click();
-  }, [topicTitle]);
-
-  const datapoints123 = [
-          { x: "2021-06-29T01:00:00", y: 530 },
-          { x: "2021-06-29T14:00:00", y: 340 },
-          { x: "2021-06-29T15:30:00", y: 510 },
-          { x: "2021-06-29T16:00:00", y: 610 },
-          { x: "2021-06-29T19:30:00", y: 111 },
-          { x: "2021-06-29T20:00:00", y: 180 },
-          { x: "2021-06-29T23:00:00", y: 290 },
-          { x: "2021-06-29T23:50:00", y: 100 },
-  ];
-
-  // this is the x value 
-  const dataPoints = [1, 2, 3, 4, 5, 6, 7, 8];
-
-  // this is the y value
-  const dates = ['2022-08-14', '2022-08-15','2022-08-16', '2022-08-17','2022-08-18', '2022-08-19','2022-08-20', '2022-08-21',];
-
-  const [graphXData, setGraphXData] = useState(dates);
-  const [graphYData, setGraphYData] = useState(dataPoints);
-  const [startDate, setStartDate] = useState(defaultStartDate);
-  const [endDate, setEndDate] = useState(defaultEndDate);
+  const [dataPoints, setDataPoints] = useState([
+    { x: "2022-08-14T00:00:00", y: 530 },
+    { x: "2022-08-14T04:00:00", y: 340 },
+    { x: "2022-08-14T15:30:00", y: 510 },
+    { x: "2022-08-14T16:00:00", y: 610 },
+    { x: "2022-08-14T19:30:00", y: 111 },
+    { x: "2022-08-14T20:00:00", y: 180 },
+    { x: "2022-08-14T23:00:00", y: 290 },
+    { x: "2022-08-14T23:50:00", y: 100 },
+  ]);
 
   const data = {
     // labels: graphXData,
     datasets: [
       {
-        label: topicTitle,
-        data: datapoints123,
+        label: "THRESHOLD PASSED",
+        data: dataPoints,
         backgroundColor: "rgba(54, 162, 235, 0.5)",
         borderColor: "rgb(54, 162, 235)",
         cubicInterpolationMode: "monotone",
         fill: true,
+        tension: 0.4,
       },
     ],
   };
 
   const options = {
+    interaction: {
+      intersect: false,
+      mode: "index",
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: topicTitle,
+      },
+      datalabels: {
+        backgroundColor: (context) => context.dataset.borderColor,
+        padding: 4,
+        borderRadius: 4,
+        clip: true,
+        color: "white",
+        font: {
+          weight: "bold",
+        },
+        formatter: (value) => value.y,
+      },
+    },
     scales: {
       x: {
         type: "time",
         time: {
           unit: "hour",
+        },
+        ticks: {
+          major: {
+            enabled: true,
+          },
+          font: (context) => {
+            const boldedTicks =
+              context.tick && context.tick.major ? "bold" : "";
+            return { weight: boldedTicks };
+          },
         },
         title: {
           display: true,
@@ -81,36 +91,38 @@ function Graph({ topicTitle, topicThreshold }) {
   };
 
   function filterData() {
-    const dates2 =[...dates];
-    console.log(dates2);
-    const startDate = document.getElementById("startdate");
-    const endDate = document.getElementById("enddate");
+    const selectedDate1 = document.getElementById("startdate");
+    setSelectedDate(selectedDate1.value);
+    console.log(selectedDate1.value);
 
-    // get the index number in array
-    const indexStartDate = dates2.indexOf(startDate.value);
-    const indexEndDate = dates2.indexOf(endDate.value);
-
-    setStartDate(startDate.value);
-    setEndDate(endDate.value);
-
-    // slice the array only showing the selected section
-    const filterDate = dates2.slice(indexStartDate, indexEndDate + 1 );
-
-    data.labels = filterDate;
-    console.log(filterDate);
-    setGraphXData(filterDate);
-
-    // datapoints
-    const dataPoints2 =[...dataPoints];
-    const filterDataPoints = dataPoints2.slice(indexStartDate, indexEndDate + 1 );
-
-    data.datasets[0].data = filterDataPoints;
-    setGraphYData(filterDataPoints);
+    //TODO: AXIOS POST
+    //setDataPoints()
   }
 
   useEffect(() => {
-    
-  }, [])
+    // date formate
+    function formatDate() {
+      var d = new Date(),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = d.getFullYear();
+
+      if (month.length < 2) month = "0" + month;
+      if (day.length < 2) day = "0" + day;
+
+      return [year, month, day].join("-");
+    }
+
+    setSelectedDate(formatDate);
+  }, []);
+
+  // to export the graph into image
+  const downloadGraph = useCallback(() => {
+    const link = document.createElement("a");
+    link.download = topicTitle + ".jpeg";
+    link.href = ref.current.toBase64Image("image/jpeg", 1);
+    link.click();
+  }, [topicTitle]);
 
   return (
     <div className="chart-background">
@@ -123,22 +135,13 @@ function Graph({ topicTitle, topicThreshold }) {
           </div>
           <div className="monitor-button-div">
             <div className="filter-div">
-              {/* <button className="monitor-button">
-                      Filter Chart
-                    </button> */}
               <input
-                onChange={filterData}
+                onChange={setSelectedDate}
                 type="date"
                 id="startdate"
-                value={startDate}
+                value={selectedDate}
               ></input>
-              <input
-                onChange={filterData}
-                type="date"
-                id="enddate"
-                value={endDate}
-                min={startDate}
-              ></input>
+              <button className="monitor-button" onClick={filterData}>Filter Chart</button>
             </div>
             <div className="export-div">
               <button className="monitor-button" onClick={downloadGraph}>
